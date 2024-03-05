e1e8985fa948ecc9f6921a881aa024430eda9ae8
Pavel Yevtukhov <pavlo.yevtukhov@globallogic.com>
2024-03-05 13:16:34 +0200

    demo app update

--- ADPlayerBasicSample/ADPlayerBasicSample/Interstitial/InterstitialExampleVC.swift
+++ ADPlayerBasicSample/ADPlayerBasicSample/Interstitial/InterstitialExampleVC.swift
@@ -12,12 +12,26 @@ final class InterstitialExampleVC: UIViewController {
     private let publisherId: String
     private let tagId: String
     private var playerTag: AdPlayerTag?
-    
+    private var inProgress = false {
+        didSet {
+            stackView.arrangedSubviews.forEach {
+                if let control = $0 as? UIControl {
+                    control.isEnabled = !inProgress
+                }
+            }
+        }
+    }
+
     private lazy var statusLabel: UILabel = {
         let label = UILabel()
         label.textAlignment = .center
-        label.textColor = .blue
         return label
+    }()
+
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        return stackView
     }()
 
     private lazy var showButton: UIButton = {
@@ -27,18 +41,36 @@ final class InterstitialExampleVC: UIViewController {
         return button
     }()
 
+    private lazy var preloadAndShowButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Preload and Show Interstitial", for: .normal)
+        button.addTarget(self, action: #selector(onPreloadAndShowButtonTap), for: .touchUpInside)
+        return button
+    }()
+
     init(publisherId: String, tagId: String) {
         self.publisherId = publisherId
         self.tagId = tagId
         super.init(nibName: nil, bundle: nil)
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(stackView)
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        stackView.addArrangedSubview(showButton)
+        stackView.addArrangedSubview(preloadAndShowButton)
+
         statusLabel.text = "Loading Tag..."
 
         statusLabel.translatesAutoresizingMaskIntoConstraints = false
@@ -49,20 +81,17 @@ final class InterstitialExampleVC: UIViewController {
             statusLabel.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor)
         ])
 
-        showButton.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(showButton)
-        NSLayoutConstraint.activate([
-            showButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            showButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
+        fetchPlayerTag()
+    }
 
-        showButton.isEnabled = false
+    private func fetchPlayerTag() {
+        inProgress = true
         AdPlayer.initializePublisher(publisherId: publisherId, tagId: tagId) { [weak self] result in
             guard let self = self else { return }
             switch result {
             case .success(let playerTag):
                 self.playerTag = playerTag
-                showButton.isEnabled = true
+                inProgress = false
                 statusLabel.text = "Ready to load ADs"
             case .failure(let error):
                 print(error.localizedDescription)
@@ -72,9 +101,15 @@ final class InterstitialExampleVC: UIViewController {
 
     @objc
     private func onShowButtonTap() {
-        guard let playerTag = playerTag else { return }
+        onAdsReady()
+    }
 
+    @objc
+    private func onPreloadAndShowButtonTap() {
+        guard let playerTag = playerTag else { return }
+        inProgress = true
         statusLabel.text = "Preloading ..."
+        playerTag.invalidatePreloadCache() // needed when it's interstitial
         playerTag.preload { [weak self] error in
             guard let self = self else { return }
             onStartLoadingAds(attempt: 0)
@@ -85,13 +120,13 @@ final class InterstitialExampleVC: UIViewController {
         guard let playerTag = playerTag else { return }
         guard attempt < 10 else {
             statusLabel.text = "No Ads"
+            inProgress = false
             return
         }
 
         statusLabel.text = "Waiting for Ads ready. Attempt: \(attempt)..."
         playerTag.getReadyAdsCount { [weak self] count in
             guard let self = self else { return }
-
             if count > 0 {
                 onAdsReady()
             } else {
@@ -105,7 +140,7 @@ final class InterstitialExampleVC: UIViewController {
     private func onAdsReady() {
         guard let playerTag = playerTag else { return }
         statusLabel.text = ""
-
+        inProgress = false
         let interstitialBuilder = playerTag.asInterstitial()
         interstitialBuilder.launch()
     }
